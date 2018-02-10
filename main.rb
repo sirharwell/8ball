4264d6076bb561c567aeb9bcbfb9101771f409e9
Ian Harwell <sirharwell@gmail.com>
2018-02-09 17:43:01 -0700

    main update

--- main.rb
+++ main.rb
@@ -1,11 +1,12 @@
-require 'pry'
+require "pry"
+
 @answer = ["yes", "no", "maybe so", "ask again", "doubt it", "what do you think?", "I don't see that happening"]
 
 def ask
   puts ""
   puts ""
   puts "Welcome to the Magic 8 Ball!"
-puts "please ask a question or say exit to leave"
+puts "please ask a question or say exit to leave or edit to make changes"
 puts ""
 answer
 end
@@ -22,10 +23,8 @@ def answer
   answer = gets.strip.downcase
     if answer == "exit"
       exit
-    elsif answer == "add"
-      newanswer = gets.strip
-      @answer << newanswer
-      askagain
+    elsif answer == "edit"
+      edit
     else
       puts ""
     end
@@ -36,4 +35,37 @@ puts""
 askagain
 end
 
-ask
+
+
+def edit
+  puts ""
+  puts "Type ADD to add additional words, SEE ALL to view answers, or BACK to go back"
+  function = gets.strip.upcase
+  case function
+    when "ADD"
+      dup_answer
+      edit
+    when "SEE ALL"
+      puts @answer
+      edit
+    when "BACK"
+      ask
+    else
+      puts "Try Again"
+        edit
+    end
+  end
+
+
+  def dup_answer
+    newanswer = gets.strip
+     # @answer.each do |item|
+     #     if item == newanswer
+     #         puts "That answer already exists!"
+     #       else
+             @answer << newanswer
+       # end
+  # end
+end
+
+  ask
